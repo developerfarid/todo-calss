2a691216667cc694b6c6e029ce42094894fd37bc
developerfarid <pustfarid333@gmail.com>
2024-08-02 12:23:09 +0600

    a

--- src/component/TaskBoard.jsx
+++ src/component/TaskBoard.jsx
@@ -1,136 +1,130 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 
-import TaskList from './TaskList';
-import Modal from './Modal';
-const dataArray =[
-    {
-        id:crypto.randomUUID(),
-        title:"Learn React",
-        details:"Learn React Learn React Learn React",
-        fav:false,
-        poiroty:"High",
-        tag:["React", "JS"]
+import AddTaskModal from "./AddTaskModal";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+import NoTasksFound from "./NoTasksFound";
 
-
-    },
-    {
-        id:crypto.randomUUID(),
-        title:"Learn React",
-        details:"Learn React Learn React Learn React",
-        fav:true,
-        poiroty:"High",
-        tag:["React", "JS"]
-
-
-    },
-]
 export default function TaskBoard() {
-    const [taskData, setTaskData]=useState(dataArray)
-    console.log("hello");
-    const [edit,setEdit]= useState(false)
-    const [showModal, setShowModal]=useState(false)
-    const [editTask, setEditTask]=useState(null)
-   console.log(Object.is(null), "null"); 
-    const [task, setTask]=  useState(editTask != null ?  {
-        id:crypto.randomUUID(),
-        title:"",
-        details:"",
-        fav:false,
-        poiroty:"",
-        tag:[]
-    })
-    console.log(editTask,"edit", task);
-    console.log(task,"taskk");
-    
-    const handleChange=(e)=>{
-e.preventDefault()
-const property= [e.target.name]
-const val= e.target.value
-
-if ([e.target.name] =="tag") {
-    const newtagArray = e.target.value
-    setTask({...task, [e.target.name]:newtagArray.split(",")})
-    
-} else{
-    setTask({...task, [e.target.name]:e.target.value})
-}
+    const defaultTask = {
+        id: crypto.randomUUID(),
+        title: "Learn React Native",
+        description:
+            "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+        tags: ["web", "react", "js"],
+        priority: "High",
+        isFavorite: true,
+    };
+    const [tasks, setTasks] = useState([defaultTask]);
+    const [showAddModal, setShowAddModal] = useState(false);
+    const [taskToUpdate, setTaskToUpdate] = useState(null);
 
 
-    }
-    console.log(task);
-
-
-    console.log(dataArray);
-    const onSubmitHandler=(e)=>{
-        e.preventDefault() 
-        if (edit) {
-             setTaskData(taskData.map((item)=>{
-if (item.id == task.id) {
-    return task
-}else {
-    return item
-}
-             }))
-        }else{
-            setTaskData([...taskData, task])
+    function handleAddEditTask(newTask, isAdd) {
+        if (isAdd) {
+            setTasks([...tasks, newTask]);
+        } else {
+            setTasks(
+                tasks.map((task) => {
+                    if (task.id === newTask.id) {
+                        return newTask;
+                    }
+                    return task;
+                })
+            );
         }
-        
-setShowModal(false)
-    }
-    const handleDetate=(id)=>{
-console.log(id);
-        const afterFillter=taskData.filter((todo)=> todo.id != id)
-        setTaskData(afterFillter)
-        setShowModal(false)
-    }
-    const handleDetateAll=()=>{
-  setTaskData([])
-    }
-    const handleEdit=(todo)=>{
-        console.log(todo);
-        setEdit(true)
-        setEditTask(todo)
-        setShowModal(true)
-    }
-    console.log(task, "task");
-  return (
-    <>
-    <div>
-        {/* main task board */}
-        {/* search box */}
-        <input type="search" name="" id="" />
-        {/* action wrap */}
-        <div>
-            <button onClick={()=> setShowModal(true)}>Add New Task</button>
-            <button onClick={handleDetateAll}>Detate all</button>
-        </div>
-        {/* tasks list */}
-        <table>
-            <thead>
-                <tr>
-                    <td>fav</td>
-                    <td>Title</td>
-                    <td>Details</td>
-                    <td>Poiroty</td>
-                    <td>Tag</td>
-                    <td>Action</td>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    taskData?.map((todo)=><TaskList key={todo?.id} todo={todo} handleDetate={handleDetate} handleEdit={handleEdit} />)
-                }
-           
-            </tbody>
-        </table>
 
+       handleCloseClick();
+    }
 
+    function handleEditTask(task) {
+        setTaskToUpdate(task);
+        setShowAddModal(true);
+    }
 
-    </div>
-    {/*background */}
-<div></div>
-{showModal && <Modal task={task} handleChange={handleChange} onSubmitHandler={onSubmitHandler} />}
+    function handleDeleteTask(taskId) {
+        const tasksAfterDelete = tasks.filter((task) => task.id !== taskId);
+        setTasks(tasksAfterDelete);
+    }
 
-    </>
-  )
+    function handleDeleteAllClick() {
+        tasks.length = 0;
+        setTasks([...tasks]);
+    }
+
+    function handleFavorite(taskId) {
+        // This portion of the commented code is not fully perfect. Here
+        // we are not doing the deep cloning of the tasks array. The tasks array has
+        // objects inside, while using the spread operator, it will only make the shallow copy.
+        // But we need to do the deep copy.
+
+        // We are not removing this commented code as it was part of the recording.
+        // But the same code is now made better and written below.
+        /*
+        const taskIndex = tasks.findIndex((task) => task.id === taskId);
+        const newTasks = [...tasks];
+        newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+        setTasks(newTasks);
+        */
+
+        // The better way of managing updates in the object within an array as a
+        // state in react.
+        setTasks(tasks.map((task) => {
+            if (task.id === taskId) {
+                return {...task, isFavorite: !task.isFavorite};
+            } else {
+                return task;
+            }
+        }))
+    }
+
+    function handleSearch(searchTerm) {
+        console.log(searchTerm);
+
+        const filtered = tasks.filter((task) =>
+            task.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        setTasks([...filtered]);
+
+    }
+
+    function handleCloseClick() {
+        setShowAddModal(false);
+        setTaskToUpdate(null);
+    }
+
+    return (
+        <section className="mb-20" id="tasks">
+            {showAddModal && (
+                <AddTaskModal
+                    onSave={handleAddEditTask}
+                    onCloseClick={handleCloseClick}
+                    taskToUpdate={taskToUpdate}
+                />
+            )}
+            <div className="container">
+                <div className="p-2 flex justify-end">
+                    <SearchTask onSearch={handleSearch} />
+                </div>
+
+                <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+                    <TaskActions
+                        onAddClick={() => setShowAddModal(true)}
+                        onDeleteAllClick={handleDeleteAllClick}
+                    />
+                    {
+                        tasks.length > 0 ?
+                        (<TaskList
+                            tasks={tasks}
+                            onEdit={handleEditTask}
+                            onDelete={handleDeleteTask}
+                            onFav={handleFavorite}
+                        />) : (<NoTasksFound />)
+                    }
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/component/TaskList.jsx
+++ src/component/TaskList.jsx
@@ -1,20 +1,84 @@
-import React from 'react'
-import { FaBeer } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
-export default function TaskList({todo, handleEdit, handleDetate}) {
-
-  return (
-    <tr>
-    <td> {todo?.fav ? <FaBeer color="yellow" /> :<FaBeer color="gray" /> }  </td>
-    <td>{todo?.title}</td>
-    <td>{todo?.details}</td>
-    <td>{todo?.poiroty}</td>
-    <td>{todo?.tag?.map((t)=> {
-        return (
-         <button>{t}</button>   
-        )
-    })}</td>
-    <td><button onClick={()=> handleEdit(todo)}>Edit</button> <button onClick={()=>handleDetate(todo.id)}>Delate</button></td>
-</tr>
-  )
+export default function TaskList({ tasks, onEdit, onDelete, onFav }) {
+    return (
+        <div className="overflow-auto">
+            <table className="table-fixed overflow-auto xl:w-full">
+                <thead>
+                    <tr>
+                        <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
+                        <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
+                            {" "}
+                            Title{" "}
+                        </th>
+                        <th className="p-4 pb-8 text-sm font-semibold capitalize w-full">
+                            {" "}
+                            Description{" "}
+                        </th>
+                        <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
+                            {" "}
+                            Tags{" "}
+                        </th>
+                        <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+                            {" "}
+                            Priority{" "}
+                        </th>
+                        <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+                            {" "}
+                            Options{" "}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map((task) => (
+                        <tr
+                            key={task.id}
+                            className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
+                            <td>
+                                <button onClick={() => onFav(task.id)}>
+                                    {task.isFavorite ? (
+                                        <FaStar color="yellow" />
+                                    ) : (
+                                        <FaStar color="gray" />
+                                    )}
+                                </button>
+                            </td>
+                            <td>{task.title}</td>
+                            <td>
+                                <div>{task.description}</div>
+                            </td>
+                            <td>
+                                <ul className="flex justify-center gap-1.5 flex-wrap">
+                                    {task.tags.map((tag) => (
+                                        <li key={tag}>
+                                            <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
+                                                {tag}
+                                            </span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </td>
+                            <td className="text-center">{task.priority}</td>
+                            <td>
+                                <div className="flex items-center justify-center space-x-3">
+                                    <button
+                                        className="text-red-500"
+                                        onClick={() => onDelete(task.id)}
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        className="text-blue-500"
+                                        onClick={() => onEdit(task)}
+                                    >
+                                        Edit
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
